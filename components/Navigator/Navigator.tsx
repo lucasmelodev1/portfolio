3a9e1a62ce5aac223a6d3bd85bb7903cc92a4f71
musicianrpr <lucasgabrielmelorodrigues@gmail.com>
2022-10-12 13:31:03 -0300

    changed hero appearance

--- components/Navigator/Navigator.tsx
+++ components/Navigator/Navigator.tsx
@@ -1,4 +1,4 @@
-import React, {MutableRefObject, Ref, useContext, useEffect} from 'react'
+import React, {MutableRefObject, Ref, useContext, useEffect, useRef} from 'react'
 import styles from './Navigator.module.sass'
 import {RefContext} from "../../pages";
 import ScrollToPlugin from 'gsap/dist/ScrollToPlugin'
@@ -6,10 +6,65 @@ import { gsap } from 'gsap'
 
 export default function Navigator() {
   const { refs } = useContext(RefContext);
+  const navigator = useRef(null)
 
   useEffect(() => {
     gsap.registerPlugin(ScrollToPlugin)
   }, [])
+
+  useEffect(() => {
+    const ctx = gsap.context(() => {
+      for (let i = 0; i < 3; i++) {
+        gsap.from(
+          `#element${i+1}`, {
+            x: 300,
+          }
+        )
+
+        gsap.to(
+          `#element${i+1}`, {
+            x: 0,
+            duration: .6,
+          }
+        )
+      }
+
+      return () => ctx.revert()
+    })
+
+
+
+    return () => {
+
+    }
+  }, [navigator]);
+
+
+  const onMouseOver = (index: number) => {
+    gsap.to(
+      `#box${index}`, {
+        width: '100%',
+      }
+    )
+    gsap.to(
+      `#element${index}`, {
+        color: '#FFFFFF'
+      }
+    )
+  }
+
+  const onMouseOut = (index: number) => {
+    gsap.to(
+      `#box${index}`, {
+        width: '0',
+      }
+    )
+    gsap.to(
+      `#element${index}`, {
+        color: '#3B3B3B'
+      }
+    )
+  }
 
   const scrollTo = (ref: MutableRefObject<any>) => {
     gsap.to(
@@ -24,16 +79,36 @@ export default function Navigator() {
   }
 
   return (
-    <div className={styles.Navigator}>
-      <div className={styles.element} onClick={() => {
+    <div className={styles.Navigator} ref={navigator}>
+      <div className={styles.element}
+           id="element1"
+           onMouseOver={() => onMouseOver(1)}
+           onMouseOut={() => onMouseOut(1)}
+           onClick={() => {
         // @ts-ignore
         scrollTo(refs.about)
-      }}>WORK</div>
-      <div className={styles.divisor}/>
-      <div className={styles.element} onClick={() => {
+      }}>
+        WORK
+        <div className={styles.box} id="box1"/>
+      </div>
+      <div className={styles.element}
+           id="element2"
+           onMouseOver={() => onMouseOver(2)}
+           onMouseOut={() => onMouseOut(2)}>
+        ABOUT
+        <div className={styles.box} id="box2"/>
+      </div>
+      <div className={styles.element + ' ' + styles.contact}
+           id="element3"
+           onMouseOver={() => onMouseOver(3)}
+           onMouseOut={() => onMouseOut(3)}
+           onClick={() => {
         // @ts-ignore
         scrollTo(refs.contact)
-      }}>CONTACT</div>
+      }}>
+        CONTACT
+        <div className={styles.box} id="box3"/>
+      </div>
     </div>
   )
 }
--- screens/Hero/Hero.tsx
+++ screens/Hero/Hero.tsx
@@ -1,39 +1,104 @@
-import React, {useEffect, useRef} from 'react'
+import React, {useEffect, useRef, useState} from 'react'
 import Navigator from '../../components/Navigator/Navigator'
-import PrimaryButton from '../../components/PrimaryButton/PrimaryButton'
 import TextPlugin from 'gsap/dist/TextPlugin';
 import styles from './Hero.module.sass'
-import { gsap } from 'gsap'
+import { gsap, Power0 } from 'gsap'
 
 export default function Hero() {
-  const cursor = useRef(null)
-  const text = useRef(null)
-  const words = ['A Front-end Developer.', 'A Good Teammate.', 'Your next co-worker.']
+  const hero = useRef(null)
+  const timeline = useRef(gsap.timeline())
+  const words = ['A Developer.', 'A Leader.', 'Lucas Gabriel.']
+
+  useEffect(() => {
+    const ctx = gsap.context(() => {
+      const tl = gsap.timeline({paused: true})
+      timeline.current = tl
+
+      tl.to(
+        "#hero-background", {
+          clipPath: 'polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)',
+          height: '100%',
+          duration: .6
+        }
+      )
+      tl.to(
+        "#hero-background", {
+          duration: 1,
+          width: '100%',
+          right: 0
+        }
+      )
+      tl.to(
+        "#hero-navigator", {
+          x: document.getElementById("hero-navigator")?.clientWidth,
+          duration: .6
+        }, "<+=10%"
+      )
+      tl.to(
+        "#hero-description", {
+          text: '',
+          duration: .6
+        }, "<+=10%"
+      )
+      tl.to(
+        "#hero-background2", {
+          backgroundColor: '#101010',
+          duration: .2
+        }
+      )
+      tl.to(
+        "#background2-text", {
+          text: 'Cosmic Cliffs',
+          duration: 1
+        }
+      )
+      tl.to(
+        "#hero-description", {
+          color: '#FFFFFF',
+          fontWeight: 400,
+        }
+      )
+      tl.to(
+        "#hero-description", {
+          text: "My passion is to study about mathematics, physics, psychology, music and software development. I am a very talkative person when in a deep discussion and around people I treasure.",
+          duration: 3
+        }, "<+=10%"
+      )
+      tl.to(
+        "#background2-text2", {
+          text: 'By James Webb',
+          duration: 1
+        }, "<+=10%"
+      )
+    })
+    return () => ctx.revert()
+  }, [])
 
   useEffect(() => {
     gsap.registerPlugin(TextPlugin)
 
     gsap.to(
-      cursor.current, 1, {
+      ".cursor", 1, {
         opacity: 0,
         ease: 'back.inOut',
         repeat: -1
       }
     )
+  }, [hero])
 
+  useEffect(() => {
     let masterTl = gsap.timeline({
-      repeat: -1,
-      repeatDelay: 1,
     })
 
-    words.forEach(word => {
+    words.forEach((word, index) => {
+      const condition = index + 1 == words.length
       let tl = gsap.timeline({
-        repeat: 1,
+        repeat: condition ? 0 : 1,
         repeatDelay: .5,
-        yoyo: true
+        yoyo: condition ? false : true
       })
       tl.to(
-        text.current, {
+        "#hero-text", {
           duration: 2,
           text: word,
           delay: .5
@@ -41,19 +106,48 @@ export default function Hero() {
       )
       masterTl.add(tl)
     })
-  }, [text, words, cursor])
+  }, [hero, words])
+
+  useEffect(() => {
+    const tl = gsap.timeline({repeat: -1})
+    tl.to(
+      "#hero-background", {
+        backgroundPositionX: `calc(-11520px)`,
+        ease: Power0.easeNone,
+        duration: 300
+      }
+    )
+  }, [hero]);
+
 
   return (
-    <div className={styles.Hero}>
-      <Navigator/>
+    <div className={styles.Hero} ref={hero}>
       <div className={styles.titleDiv}>
-        <h1 className={styles.title}>Lucas Gabriel</h1>
-        <div>
-          <h2 className={styles.subtitle} ref={text}>~/</h2>
-          <span className={styles.span} ref={cursor}>_</span>
+        <h1 className={styles.title}>Hi, {"I'm"}</h1>
+        <div className={styles.subtitleDiv}>
+          <span className={styles.subtitle} id="hero-text"></span>
+          <span className={styles.subtitle + ' ' + "cursor"}>_</span>
         </div>
+        <p className={styles.description} id="hero-description">
+          I am a developer with years of experience building websites from design to deploy. Living in Brazil, I work remotely to national and international companies to make their goals come true.
+        </p>
       </div>
-      <PrimaryButton text='HIRE ME'/>
+      <div className={styles.background} id="hero-background" />
+      <div className={styles.background2} id="hero-background2" onMouseOver={() => {
+        timeline.current.play()
+      }} onMouseOut={() => {
+        if(timeline.current.totalProgress() > .95) {
+          timeline.current.reverse(2)
+        } else {
+          timeline.current.reverse()
+        }
+      }}>
+        <h3 className={styles.text} id="background2-text"></h3>
+        <h4 className={styles.text2} id="background2-text2"></h4>
+      </div>
+      <div className={styles.navigatorDiv} id="hero-navigator">
+        <Navigator/>
+      </div>
     </div>
   )
 }
